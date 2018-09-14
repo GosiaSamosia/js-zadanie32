7ed1a8d57fe8d3a21d1b22f212c88ec180ee6b17
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-14 10:37:44 +0200

    zwróć większą liczbę

---
+++ main.js
@@ -0,0 +1,13 @@
+var num1 = 2;
+var num2 = 5;
+
+function multiply(num1,num2) {
+    var result
+        if (num1>num2){
+            console.log(num1);
+        }else{
+            console.log(num2);
+        }
+    return result;
+}
+multiply(); 
